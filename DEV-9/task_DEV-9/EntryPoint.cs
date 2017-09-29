fc0ed94b7b96a03ebe4c3a72c3626b8256e6ab3f
HelenPribysh <helenpribysh@gmail.com>
2017-09-29 22:13:47 +0300

    there is a programm whitch change some letters

---
+++ DEV-9/task_DEV-9/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_9
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string firstString = "there is some kind of text";
+                string secondString = "I think I can leave my university -_-";
+                string thirdString = new StringReplacer().stringReplace(firstString, secondString);
+                Console.WriteLine(firstString);
+                Console.WriteLine(secondString);
+                Console.WriteLine(thirdString);
+            }
+            catch (Exception) { }
+        }
+    }
+}
---
+++ DEV-9/task_DEV-9/StringReplacer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_9
+{
+    class StringReplacer
+    {
+        public string stringReplace(string text1, string text2)
+        {
+            Random random = new Random();
+            int indexOfElementInFirstString = random.Next(0, text1.Length - 1);
+            int indexOfElementInSecondString = random.Next(0, text2.Length - 1);
+            int numberOfElementsInFirstString = random.Next(1, text1.Length - indexOfElementInFirstString);
+            int numberOfElementsInSecondString = random.Next(1, text2.Length - indexOfElementInSecondString);
+            string removedElementsFromFirstString = text1.Substring(indexOfElementInFirstString, numberOfElementsInFirstString);
+            string removedElementsFromSecondString = text2.Substring(indexOfElementInSecondString, numberOfElementsInSecondString);
+            text1 = text1.Replace(removedElementsFromFirstString, removedElementsFromSecondString);
+            return text1;
+        }
+    }
+}
