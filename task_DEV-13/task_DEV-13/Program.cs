83ad989fd2e7da10019579ed0bf69017899e3fdf
HelenPribysh <helenpribysh@gmail.com>
2017-11-08 15:19:35 +0300

    I'm SO SORRY you watched this, but I had to show that I tried

---
+++ task_DEV-13/task_DEV-13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             List<Qualification> baseQualification = new List<Qualification>()
+       {
+           new Qualification(300, 1, "jun"), 
+           new Qualification(400,3,"middle"),
+           new Qualification(550,5,"senior"),
+           new Qualification(700,7,"lead") 
+       };
+            List<List<Qualification>> combinationOfQualification = new List<List<Qualification>>(1)
+        {
+            {new List<Qualification>(){baseQualification[0]}},
+            {new List<Qualification>(){baseQualification[1]}},
+            {new List<Qualification>(){baseQualification[2]}},
+            {new List<Qualification>(){baseQualification[3]}},
+            {new List<Qualification>(){baseQualification[0], baseQualification[1]}},
+            {new List<Qualification>(){baseQualification[0], baseQualification[2]}},
+            {new List<Qualification>(){baseQualification[0], baseQualification[3]}},
+            {new List<Qualification>(){baseQualification[1], baseQualification[2]}},
+            {new List<Qualification>(){baseQualification[1], baseQualification[3]}},
+            {new List<Qualification>(){baseQualification[2], baseQualification[3]}},
+            {new List<Qualification>(){baseQualification[0], baseQualification[1], baseQualification[2]}},
+            {new List<Qualification>(){baseQualification[0], baseQualification[1], baseQualification[3]}},
+            {new List<Qualification>(){baseQualification[1], baseQualification[2], baseQualification[3]}},
+            {new List<Qualification>(){baseQualification[0], baseQualification[2], baseQualification[3]}},
+            {baseQualification}
+        };
+            string entryBudget = Console.ReadLine();
+            FirstCriterium firstCriterium = new FirstCriterium(Convert.ToInt32(entryBudget), combinationOfQualification);
+            Console.WriteLine(firstCriterium.algoritm());
+        }
+    }
+}
---
+++ task_DEV-13/task_DEV-13/Qualification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_13
+{
+     class Qualification
+    {
+         public int salary { get; set; }
+         public double productivity { get; set; }
+         public string title { get; set; }
+         public Qualification(int salary, double productivity, string title)
+         {
+             this.salary = salary;
+             this.productivity = productivity;
+             this.title = title;
+         }
+    }
+}
